3ecb797d7e88d3ad1f18d8bee865c5771d357c6b
leslientodi <lesliehazard500@gmail.com>
2024-06-12 07:52:15 +0000

    you won!

--- dice-app/src/App.js
+++ dice-app/src/App.js
@@ -5,21 +5,45 @@ import {nanoid} from "nanoid"
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+
+
+  React.useEffect(()=> {
+    const allHeld = dice.every(dice => dice.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(dice => dice.value=== firstValue)
+    if (allHeld && allSameValue){
+      setTenzies(true)
+      console.log("You won")
+    }
+    
+  }, [dice])
 
   function allNewDice()  {
     const newDice = []
     for (let i = 0; i<10; i++) {
-      newDice.push({
-        value:Math.ceil(Math.random()*6),
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateNewDice())
     }
     return newDice
   }
 
+  function generateNewDice() {
+    return {
+        value:Math.ceil(Math.random()*6),
+        isHeld: false,
+        id: nanoid()
+    }
+  }
+
+
+
+
+
   function rollDice() {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? 
+      die : generateNewDice()
+    } ))
   }
 
   function holdDice (id) {
@@ -38,6 +62,10 @@ function App() {
 
   return (
     <main>
+      <h1 className='title'>Tenzies</h1>
+      <p className='instructions'>Roll until all dice are the same. Click each die to freeze 
+        it at its current value between rolls
+      </p>
       <div className='dice-container'>
         {diceElements}
       
